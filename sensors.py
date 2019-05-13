447e5655e1c6c3f114317185563c26a17d80741c
Matthew Wilkes <git@matthewwilkes.name>
2019-05-13 14:05:54 +0100

    Add raspberrypi specific sensor

--- sensors.py
+++ sensors.py
@@ -22,7 +22,7 @@ def ip_addresses():
 
 
 def cpu_load():
-    return psutil.cpu_percent(interval=0.1)
+    return psutil.cpu_percent(interval=0.1) / 100.0
 
 
 def ram_available():
@@ -33,14 +33,26 @@ def ac_connected():
     return psutil.sensors_battery().power_plugged
 
 
-@click.command(help="Displays the values of the sensors")
+def get_relative_humidity():
+    try:
+        from adafruit_dht import DHT11
+        from board import D4
+    except (ImportError, NotImplementedError):
+        # No DHT library results in an ImportError.
+        # Running on an unknown platform results in a NotImplementedError when getting the pin
+        return None
+    return DHT11(D4).humidity
+
+
+@click.command()#help="Displays the values of the sensors")
 def show_sensors():
     click.echo("Python version: {0.major}.{0.minor}".format(python_version()))
     for address in ip_addresses():
         click.echo("IP addresses: {0[1]} ({0[0]})".format(address))
-    click.echo("CPU Load: {:.1f}".format(cpu_load()))
-    click.echo("RAM Available: {} MiB".format(ram_available() / 1024**2))
-    click.echo("AC Connected: {}".format(ac_connected()))
+    click.echo("CPU Load: {:.1%}".format(cpu_load()))
+    click.echo("RAM Available: {:.0f} MiB".format(ram_available() / 1024**2))
+    click.echo("AC Connected: {!r}".format(ac_connected()))
+    click.echo("Humidity: {!r}".format(get_relative_humidity()))
 
 
 if __name__ == '__main__':
