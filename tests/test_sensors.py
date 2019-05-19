82169eff098734cf73fa26cedcfabf36248df273
Matthew Wilkes <git@matthewwilkes.name>
2019-05-19 17:30:42 +0100

    Add basic integration test for version

--- tests/test_sensors.py
+++ tests/test_sensors.py
@@ -1,4 +1,14 @@
+import sys
+
+from click.testing import CliRunner
+
 import sensors
 
 def test_sensors():
     assert hasattr(sensors, 'PythonVersion')
+
+def test_python_version_is_first_two_lines_of_cli_output():
+    runner = CliRunner()
+    result = runner.invoke(sensors.show_sensors)
+    python_version = "{}.{}".format(sys.version_info.major, sys.version_info.minor)
+    assert ["Python Version", python_version] == result.stdout.split("\n")[:2]
