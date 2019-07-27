e2328e42386edbcb91b648799566b35b9d64ee59
Matthew Wilkes <git@matthewwilkes.name>
2019-07-27 18:13:16 +0100

    Add --develop option to test a sensor

--- src/apd/sensors/cli.py
+++ src/apd/sensors/cli.py
@@ -1,3 +1,6 @@
+import enum
+import importlib
+import sys
 import typing as t
 
 import click
@@ -14,6 +17,38 @@ from .sensors import (
 )
 
 
+class ReturnCodes(enum.IntEnum):
+    OK = 0
+    BAD_SENSOR_PATH = 17
+
+
+def get_sensor_by_path(sensor_path):
+    try:
+        module_name, sensor_name = sensor_path.split(":")
+    except ValueError:
+        raise RuntimeError(
+            "Sensor path must be in the format dotted.path.to.module:ClassName"
+        )
+    try:
+        module = importlib.import_module(module_name)
+    except ImportError:
+        raise RuntimeError(f"Could not import module {module_name}")
+    try:
+        sensor_class = getattr(module, sensor_name)
+    except AttributeError:
+        raise RuntimeError(f"Could not find attribute {sensor_name} in {module_name}")
+    if (
+        isinstance(sensor_class, type)
+        and issubclass(sensor_class, Sensor)
+        and sensor_class != Sensor
+    ):
+        return sensor_class()
+    else:
+        raise RuntimeError(
+            f"Detected object {sensor_class!r} is not recognised as a Sensor type"
+        )
+
+
 def get_sensors():
     return [
         PythonVersion(),
@@ -27,11 +62,25 @@ def get_sensors():
 
 
 @click.command(help="Displays the values of the sensors")
+@click.option(
+    "--develop", required=False, metavar="path", help="Load a sensor by Python path"
+)
 def show_sensors(develop):
     sensors: t.Iterable[Sensor[t.Any]]
-    sensors = get_sensors()
+    if develop:
+        try:
+            sensors = [get_sensor_by_path(develop)]
+        except RuntimeError as error:
+            click.secho(str(error), fg="red", bold=True)
+            sys.exit(ReturnCodes.BAD_SENSOR_PATH)
+    else:
+        sensors = get_sensors()
     for sensor in sensors:
         click.secho(sensor.title, bold=True)
         click.echo(str(sensor))
         click.echo("")
-    return
+    sys.exit(ReturnCodes.OK)
+
+
+if __name__ == "__main__":
+    show_sensors()
